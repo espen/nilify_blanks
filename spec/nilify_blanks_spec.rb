244b1add56eacbf1f1b2235e10ebc1477beef6e2
Espen Antonsen <espen@inspired.no>
2017-12-25 22:25:46 +0100

    better tests for array support

--- spec/nilify_blanks_spec.rb
+++ spec/nilify_blanks_spec.rb
@@ -2,17 +2,22 @@ require "nilify_blanks/matchers"
 
 RSpec.describe NilifyBlanks do
   context "Model with nilify_blanks" do
+
+    def array_supported?
+      Post.content_columns.detect {|c| c.respond_to?(:array) && c.array?}
+    end
+
     before(:all) do
       class Post < ActiveRecord::Base
         nilify_blanks
       end
 
-      @post = Post.new(:first_name => '', :last_name => '', :title => '', :summary => '', :body => '', :slug => '', :views => 0, :blog_id => '')
+      @post = Post.new(:first_name => '', :last_name => '', :title => '', :summary => '', :body => '', :slug => '', :views => 0, :blog_id => '', :custom_data => [''])
       @post.save
     end
 
     it "should recognize all non-null string, text, citext columns" do
-      expect(Post.nilify_blanks_columns).to eq(['first_name', 'title', 'summary', 'body', 'slug', 'blog_id'])
+      expect(Post.nilify_blanks_columns).to eq(['first_name', 'title', 'summary', 'body', 'slug', 'blog_id', 'custom_data'])
     end
 
     it "should convert all blanks to nils" do
@@ -22,6 +27,7 @@ RSpec.describe NilifyBlanks do
       expect(@post.body).to be_nil
       expect(@post.slug).to be_nil
       expect(@post.blog_id).to be_nil
+      expect(@post.custom_data).to be_nil if array_supported?
     end
 
     it "should leave not-null last name field alone" do
@@ -35,6 +41,20 @@ RSpec.describe NilifyBlanks do
     it "should not nilify non-null column" do
       expect(@post.class.nilify_blanks_columns).to_not include('last_name')
     end
+
+    it "should convert all array blanks to nils" do
+      next unless array_supported?
+      @post = Post.new(:last_name => '', :custom_data => ['', 'foo', nil, 'bar'])
+      @post.save
+      @post.custom_data.length.should == 2
+    end
+
+    it "should convert empty array to nil" do
+      next unless array_supported?
+      @post = Post.new(:last_name => '', :custom_data => ['', nil, ''])
+      @post.save
+      @post.custom_data.should == nil
+    end
   end
 
   context "Model with nilify_blanks :nullables_only => false" do
@@ -44,11 +64,11 @@ RSpec.describe NilifyBlanks do
         nilify_blanks :nullables_only => false
       end
 
-      @post = PostWithNullables.new(:first_name => '', :last_name => '', :title => '', :summary => '', :body => '', :slug => '', :views => 0, :blog_id => '')
+      @post = PostWithNullables.new(:first_name => '', :last_name => '', :title => '', :summary => '', :body => '', :slug => '', :views => 0, :blog_id => '', :custom_data => [''])
     end
 
     it "should recognize all (even null) string, text, citext columns" do
-      expect(PostWithNullables.nilify_blanks_columns).to eq(['first_name', 'last_name', 'title', 'summary', 'body', 'slug', 'blog_id'])
+      expect(PostWithNullables.nilify_blanks_columns).to eq(['first_name', 'last_name', 'title', 'summary', 'body', 'slug', 'blog_id', 'custom_data'])
     end
   end
 
@@ -114,25 +134,30 @@ RSpec.describe NilifyBlanks do
     end
   end
 
-  context "Model with nilify_blanks :except => [:first_name, :title]" do
+  context "Model with nilify_blanks :except => [:first_name, :title, :blog_id]" do
+    def array_supported?
+      Post.content_columns.detect {|c| c.respond_to?(:array) && c.array?}
+    end
+
     before(:all) do
-      class PostExceptFirstNameAndTitle < ActiveRecord::Base
+      class PostExceptFirstNameAndTitleAndBlogId < ActiveRecord::Base
         self.table_name = "posts"
         nilify_blanks :except => [:first_name, :title, :blog_id]
       end
 
-      @post = PostExceptFirstNameAndTitle.new(:first_name => '', :last_name => '', :title => '', :summary => '', :body => '', :slug => '', :views => 0)
+      @post = PostExceptFirstNameAndTitleAndBlogId.new(:first_name => '', :last_name => '', :title => '', :summary => '', :body => '', :slug => '', :views => 0, :custom_data => [''])
       @post.save
     end
 
-    it "should recognize only summary, body, and views" do
-      expect(PostExceptFirstNameAndTitle.nilify_blanks_columns).to eq(['summary', 'body', 'slug'])
+    it "should recognize only summary, body, views and custom_data" do
+      expect(PostExceptFirstNameAndTitle.nilify_blanks_columns).to eq(['summary', 'body', 'slug', 'custom_data'])
     end
 
     it "should convert summary and body blanks to nils" do
       expect(@post.summary).to be_nil
       expect(@post.body).to be_nil
       expect(@post.slug).to be_nil
+      expect(@post.custom_data).to be_nil
     end
 
     it "should leave other fields alone" do
